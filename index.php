efb931db26849b36faf2ba8c9e88589843c3b22b
theanh <=>
2022-06-21 09:51:49 +0700

    index

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ if ($dangxuat == 'dangxuat') {
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-    <!-- <style>
+    <style>
     .home-filter__btn:focus{
         outline:none;
         box-shadow: none;
@@ -44,7 +44,7 @@ if ($dangxuat == 'dangxuat') {
     .category{
         padding-left: 0;
     }git 
-</style> -->
+</style> 
 
 </head>
 
